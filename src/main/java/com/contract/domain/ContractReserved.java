c7f458511adee06a8b18e871a72fc949b0f8aea9
owslove777 <owslove777@naver.com>
2022-04-22 06:51:24 +0000

    apply kafka

---
+++ src/main/java/com/contract/domain/ContractReserved.java
@@ -0,0 +1,28 @@
+package com.example.contract.domain;
+
+import com.example.contract.AbstractEvent;
+
+public class ContractReserved extends AbstractEvent {
+    Long id;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {this.id = id;}
+
+    String talentCategory;
+    public String getTalentCategory() {
+        return talentCategory;
+    }
+    public void setName(String talentCategory) {
+        this.talentCategory = talentCategory;
+    }
+
+    String requirement;
+    public String getRequirement() {
+        return requirement;
+    }
+    public void setRequirement(String requirement) {
+        this.requirement = requirement;
+    }
+
+}
---
+++ src/main/java/com/contract/domain/ContractUpdated.java
@@ -0,0 +1,7 @@
+package com.example.contract.domain;
+
+import com.example.contract.domain.ContractReserved;
+
+public class ContractUpdated extends ContractReserved{
+    
+}
